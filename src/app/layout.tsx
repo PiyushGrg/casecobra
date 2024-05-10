58bce86cd10bf3e0f690374c7280514aa002f488
Pgarg2025 <97442147+Pgarg2025@users.noreply.github.com>
2024-05-10 16:43:25 +0530

    Customer support added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
 import { Recursive } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import { constructMetadata } from "@/lib/utils";
+import Script from "next/script";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
@@ -26,6 +26,23 @@ export default function RootLayout({
           <Footer />
         </main>
         <Toaster />
+        <Script
+          id="tawk-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/663dcf659a809f19fb2f69fc/1htgn4a5o';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
